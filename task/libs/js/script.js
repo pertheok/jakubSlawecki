11ec1ab6ceab20b1ae3e12796d54d3827ab63d2c
Jakub Slawecki <kubaslawecki@gmail.com>
2022-06-07 21:41:07 +0100

    complete initial version of all files covering api 1

---
+++ task/libs/js/script.js
@@ -0,0 +1,31 @@
+$('#api-run-1').click(() => {
+
+    $.ajax({
+        url: "libs/php/getEarthquakeInfo.php",
+        type: "POST",
+        dataType: 'json',
+        data: {
+            north: $('#selNorth').val(),
+            south: $('#selSouth').val(),
+            east: $('#selEast').val(),
+            west: $('#selWest').val()
+        },
+        success: function(result) {
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#api1Datetime').html(result['data'][0]['datetime']);
+                $('#api1Depth').html(result['data'][0]['depth']);
+                $('#api1Magnitude').html(result['data'][0]['magnitude']);
+                $('#api1Latitude').html(result['data'][0]['lat']);
+                $('#api1Longitude').html(result['data'][0]['lng']);
+
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(`${textStatus}, ${errorThrown}`);
+        }
+    });
+
+});
