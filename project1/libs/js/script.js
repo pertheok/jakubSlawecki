5dd99ed4aaec7c6c0bf84ad1cd21979982bfab86
Jakub Slawecki <kubaslawecki@gmail.com>
2022-06-09 22:49:08 +0100

    initial version of project 1

---
+++ project1/libs/js/script.js
@@ -0,0 +1,8 @@
+//Leaflet configuration
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
