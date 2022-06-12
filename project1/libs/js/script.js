c90de7ae019e795411cf0eef99a38db4bdd07655
Jakub Slawecki <kubaslawecki@gmail.com>
2022-06-12 20:15:08 +0100

    add comments and some functionality throughout

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,18 +1,117 @@
 //Leaflet configuration
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//center the map on user's location
+
+map.locate({
+    setView: true,
+    maxZoom: 5
+});
+
+//drawing a polygon over the selected country
+
+//test code to see how boundaries are displayed, will be removed and replaced by a functionality that will draw a border based on user's selection
+let uk = {
+    "type":"Feature",
+    "properties":{
+        "name":"United Kingdom",
+        "iso_a2":"GB",
+        "iso_a3":"GBR",
+        "iso_n3":"826"
+    },
+    "geometry":{
+        "type":"MultiPolygon",
+        "coordinates":[
+            [
+                [
+                    [-5.661948614921897,54.55460317648385],
+                    [-6.197884894220977,53.86756500916334],
+                    [-6.953730231137996,54.073702297575636],
+                    [-7.572167934591079,54.05995636658599],
+                    [-7.366030646178785,54.595840969452695],
+                    [-7.572167934591079,55.1316222194549],
+                    [-6.733847011736145,55.1728600124238],
+                    [-5.661948614921897,54.55460317648385]
+                ]
+            ],
+            [
+                [
+                    [-3.005004848635281,58.63500010846633],
+                    [-4.073828497728016,57.55302480735525],
+                    [-3.055001796877661,57.69001902936095],
+                    [-1.959280564776918,57.68479970969951],
+                    [-2.219988165689301,56.87001740175353],
+                    [-3.119003058271118,55.973793036515474],
+                    [-2.085009324543023,55.90999848085127],
+                    [-2.005675679673857,55.80490285035023],
+                    [-1.11499101399221,54.62498647726539],
+                    [-0.4304849918542,54.46437612570216],
+                    [0.184981316742039,53.32501414653103],
+                    [0.469976840831777,52.92999949809197],
+                    [1.681530795914739,52.739520168664],
+                    [1.559987827164377,52.09999848083601],
+                    [1.050561557630914,51.806760565795685],
+                    [1.449865349950301,51.28942780212196],
+                    [0.550333693045502,50.765738837275876],
+                    [-0.78751746255864,50.77498891865622],
+                    [-2.489997524414377,50.50001862243124],
+                    [-2.956273972984036,50.696879991247016],
+                    [-3.617448085942328,50.22835561787272],
+                    [-4.542507900399244,50.34183706318566],
+                    [-5.245023159191135,49.95999990498108],
+                    [-5.776566941745301,50.15967763935682],
+                    [-4.309989793301838,51.21000112568916],
+                    [-3.414850633142123,51.42600861266925],
+                    [-3.422719467108323,51.42684816740609],
+                    [-4.984367234710874,51.593466091510976],
+                    [-5.267295701508885,51.99140045837458],
+                    [-4.222346564134853,52.301355699261364],
+                    [-4.770013393564113,52.840004991255626],
+                    [-4.579999152026915,53.49500377055517],
+                    [-3.093830673788659,53.404547400669685],
+                    [-3.092079637047106,53.404440822963544],
+                    [-2.945008510744344,53.984999701546684],
+                    [-3.614700825433034,54.600936773292574],
+                    [-3.63000545898933,54.615012925833014],
+                    [-4.844169073903004,54.790971177786844],
+                    [-5.082526617849226,55.06160065369937],
+                    [-4.719112107756644,55.50847260194348],
+                    [-5.047980922862109,55.78398550070752],
+                    [-5.586397670911139,55.31114614523682],
+                    [-5.644998745130181,56.275014960344805],
+                    [-6.149980841486354,56.78500967063354],
+                    [-5.786824713555291,57.81884837506465],
+                    [-5.009998745127575,58.63001333275005],
+                    [-4.211494513353557,58.55084503847917],
+                    [-3.005004848635281,58.63500010846633]
+                ]
+            ]
+        ]
+    }
+}
+
+//displaying the boundaries extracted from the .geo.json file on the map
+
+let currentCountry = L.geoJSON(uk).addTo(map);
+
+//displaying country information modal after clicking on the country outline
+
+currentCountry.on('click', () => {
+    $('#countryInfoModal').modal("show");
+});
+
 //search logic
 
 const search = document.getElementById("country-search");
 const matchList = document.getElementById("match-list");
 
-//search countryBorders.geo.json and filter it by user's input, going through the available country names
+//search countryBorders.geo.json and filter it by user's input, going through the list of available country names
 
 const searchCountries = async userInput => {
     const response = await fetch('./libs/json/countryBorders.geo.json');
@@ -36,13 +135,13 @@ const searchCountries = async userInput => {
     outputHtml(matches);
 }
 
-// display results in HTML
+// display results as HTML
 
 const outputHtml = matches => {
     if (matches.length > 0) {
         const html = matches.map(match => `
             <li class="list-group-item">
-                <h4>${match.properties.name} (${match.properties['iso_a2']} / ${match.properties['iso_a3']})</h4>
+                <h4>${match.properties.name}</h4>
             </li>
         `).join('');
 
@@ -52,39 +151,54 @@ const outputHtml = matches => {
 
 search.addEventListener('input', () => searchCountries(search.value));
 
-//script for handling requests
+//script for handling requests - WIP, needs some refactoring
 
-$('.list-group-item').click(() => { //needs changing
 
-    $.ajax({
-        url: "libs/php/formHandler.php",
-        type: "POST",
-        dataType: 'json',
-        data: {
-            countryCode: $('#selNorth').val() //needs changing
-        },
-        success: function(result) {
-            console.log(result);
+//below doesn't seem to work, probably because of the second .attr argument? how to make it so it targets selection's html?
+$('#matchList').on('click', '.list-group-item', () => {
+    $('#country-search').attr(value, match.properties.name);
+});
 
-            if (result.status.name == "ok") {
-                    $('#countryName').html(result.countryName);
-                    $('#capitalName').html(result.capital);
-                    $('#countryPopulation').html(result.population);
-                    $('#countryCurrency').html(result.currencyCode);
-                    $('#countryCurrencyExchange').html(result.currencyCode); //<-needs implementation
-                    $('#countryWeather').html(result.currencyCode); //<-needs implementation
+//below needs some refactoring
 
-                    if (result.countryName.includes(" ")) {
-                        let underscore = result.countryName.replace(" ", "_");
-                        $('#countryWiki').attr('href', `https://en.wikipedia.org/wiki/${underscore}`);
-                    } else {
-                        $('#countryWiki').attr('href', `https://en.wikipedia.org/wiki/${result.countryName}`); //<-needs implementation
-                    }   
+$('#country-search').keypress(event => {
+
+    if (event.which == 13) {
+
+        $.ajax({
+            url: "libs/php/formHandler.php",
+            type: "POST",
+            dataType: 'json',
+            data: {
+                countryCode: $('#selNorth').val(), //id needs changing
+                currency: $('#selNorth').val(), //id needs changing
+                capitalName: $('#selNorth').val() //id needs changing
+            },
+            success: function(result) {
+                console.log(result);
+
+                if (result.status.name == "ok") {
+                        $('#countryName').html(result.countryName);
+                        $('#capitalName').html(result.capital);
+                        $('#countryPopulation').html(result.population);
+                        $('#countryCurrency').html(result.currencyCode);
+                        $('#countryCurrencyExchange').html(result.currencyCode); //<-needs implementation based on API response
+                        $('#countryWeather').html(result.currencyCode); //<-needs implementation based on API response
+
+                        if (result.countryName.includes(" ")) {
+                            let underscore = result.countryName.replace(" ", "_");
+                            $('#countryWiki').attr('href', `https://en.wikipedia.org/wiki/${underscore}`);
+                        } else {
+                            $('#countryWiki').attr('href', `https://en.wikipedia.org/wiki/${result.countryName}`); //<-needs implementation based on API response
+                        }   
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(`${textStatus}, ${errorThrown}`);
             }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(`${textStatus}, ${errorThrown}`);
-        }
     });
 
+    }
+
 });
+
--- project1/libs/php/formHandler.php
+++ project1/libs/php/formHandler.php
@@ -5,18 +5,59 @@
 
     $executionStartTime = microtime(true);
 
-    $url = 'http://api.geonames.org/countryInfoJSON?&lang=en&country=' . $_REQUEST['countryCode'] . '&username=pertheok';
+    //URLs to send the requests to - API keys not hidden as they are free and do not incur any costs when going over limitations
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_URL, $url);
+    $countryUrl = 'http://api.geonames.org/countryInfoJSON?&lang=en&country=' . $_REQUEST['countryCode'] . '&username=pertheok'; //URL to retrieve data from the api providing basic country information
+    $currencyUrl = 'https://openexchangerates.org/api/latest.json?app_id=c692867c4e8647368b163220f42193fe&symbols=' . $_REQUEST['currency']; //URL to retrieve data from currency exchange, free account only allows to use USD as a base
+    $weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' . $_REQUEST['capitalName'] . '&APPID=e32492c31dc6e5cd00009f4f881846d6'; //URL to retrieve data from weather api
 
-    $result = curl_exec($ch);
+    //initialising a handle for each URL
 
-    curl_close($ch);
+    $ch1 = curl_init();
+    $ch2 = curl_init();
+    $ch3 = curl_init();
 
-    $decode = json_decode($result, true);
+    curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch1, CURLOPT_URL, $countryUrl);
+
+    curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch2, CURLOPT_URL, $currencyUrl);
+
+    curl_setopt($ch3, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch3, CURLOPT_URL, $weatherUrl);
+
+    //creating the multiple cURL handle
+
+    $mh = curl_multi_init();
+
+    //adding the three handles to the multiple cURL handles
+
+    curl_multi_add_handle($mh, $ch1);
+    curl_multi_add_handle($mh, $ch2);
+    curl_multi_add_handle($mh, $ch3);
+
+    // executing the handles
+
+    do {
+        $status = curl_multi_exec($mh, $active);
+        if ($active) {
+            curl_multi_select($mh);
+        }
+    } while ($active && $status == CURLM_OK);
+
+    //closing the handles
+
+    curl_multi_remove_handle($mh, $ch1);
+    curl_multi_remove_handle($mh, $ch2);
+    curl_multi_remove_handle($mh, $ch3);
+    curl_multi_close($mh);
+
+    //BELOW NEEDS REFACTORING
+
+    $decode = json_decode($status, true);
 
     $output['status']['code'] = '200';
     $output['status']['name'] = 'ok';
