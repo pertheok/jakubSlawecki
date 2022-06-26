ffad79086532b661b967989942e4cec6d35ab883
Jakub Slawecki <kubaslawecki@gmail.com>
2022-06-26 23:37:23 +0100

    create boilerplate code for php enquiries

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -3,6 +3,9 @@
 
 	// connection details for MySQL database
 
+	//for website:
+	//141.136.43.81
+
 	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
@@ -12,5 +15,9 @@
 	$cd_dbname = "companydirectory";
 	$cd_user = "root";
 	$cd_password = "";
+
+	//for website:
+	//pertheok
+	//Chapters-7
 
 ?>
